2d8984ab1648c26181c9e03043de1d52e6b16bf6
Roemer <r.baeriswyl@gmail.com>
2016-06-21 17:43:37 +0200

    Added freerdp package

---
+++ freerdp/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'freerdp'
+$url = 'https://www.cloudbase.it/downloads/wfreerdp-1.1.zip'
+$url64 = $url
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
