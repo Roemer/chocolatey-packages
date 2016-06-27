7b23733bf630a9cc41ed46bbaf53bdbb89d8f126
Roemer <r.baeriswyl@gmail.com>
2016-06-27 11:33:47 +0200

    Added SonarCube Scanner

---
+++ sonarcube-scanner/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'sonarcube-scanner'
+$url = 'https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.6.1.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage $packageName $url $toolsDir
