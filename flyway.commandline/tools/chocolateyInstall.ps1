d341cd45942be6e236b3b452b59dca89b4c36529
Roemer <r.baeriswyl@gmail.com>
2016-07-06 08:54:57 +0200

    Added flyway commandline packages

---
+++ flyway.commandline/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$version = '4.0.3'
+$packageName = 'flyway.commandline'
+$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
