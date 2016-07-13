d015d1f0277968b7a97c8172b67532641024a92b
Roemer <r.baeriswyl@gmail.com>
2016-07-13 16:47:00 +0200

    Added sqlserver cmdline and odbc driver

---
+++ sqlserver-cmdlineutils/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'sqlserver-cmdlineutils'
+$installerType = 'msi'
+$silentArgs= '/Passive /NoRestart IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES'
+$url = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x86/MsSqlCmdLnUtils.msi'
+$url64 = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x64/MsSqlCmdLnUtils.msi'
+$checksumType = 'sha256'
+$checksum = '91430BA2CD4AE04260895B1080AE7965F64AEFA46C92CE64B26BDC59602B5F4F'
+$checksum64 = '33B136C49105CCBEA415038520EFBB2370F7B4A80B20BFF47F8AE5D155BB1C5D'
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType"
---
+++ sqlserver-odbcdriver/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'sqlserver-odbcdriver'
+$installerType = 'msi'
+$silentArgs= '/Passive /NoRestart IACCEPTMSODBCSQLLICENSETERMS=YES'
+$url = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/1033/x86/msodbcsql.msi'
+$url64 = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/1033/amd64/msodbcsql.msi'
+$checksumType = 'sha256'
+$checksum = 'CE9B9DDD8F38926DF374C1B947DD8A312495E8D81BE985109347E432257BA53E'
+$checksum64 = 'CC7FD7CBB9840DE239DCC0C8AB12D2CC2BCF411AF4B0B8BD5AB0730805F2F4B3'
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType"
