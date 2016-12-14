693a5eba9e38b6daaf117632c63a720527754a9e
Roemer <r.baeriswyl@gmail.com>
2016-12-14 10:48:46 +0100

    Updated SQL and Sonar Packages

--- sonarcube-scanner/tools/chocolateyinstall.ps1
+++ sonarcube-scanner/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'sonarcube-scanner'
-$url = 'https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.6.1.zip'
+$url = 'https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.8.zip'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage $packageName $url $toolsDir
-Install-BinFile "sonar-runner" "$toolsDir\sonar-scanner-2.6.1\bin\sonar-runner.bat"
-Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-2.6.1\bin\sonar-scanner.bat"
+Install-BinFile "sonar-runner" "$toolsDir\sonar-scanner-2.8\bin\sonar-runner.bat"
+Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-2.8\bin\sonar-scanner.bat"
--- sqlserver-cmdlineutils/tools/chocolateyinstall.ps1
+++ sqlserver-cmdlineutils/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'sqlserver-cmdlineutils'
 $installerType = 'msi'
 $silentArgs= '/Passive /NoRestart IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES'
-$url = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x86/MsSqlCmdLnUtils.msi'
-$url64 = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x64/MsSqlCmdLnUtils.msi'
+$url = 'https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/Command%20Line%20Utilities%20MSI%20files/x86/MsSqlCmdLnUtils.msi'
+$url64 = 'https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/Command%20Line%20Utilities%20MSI%20files/amd64/MsSqlCmdLnUtils.msi'
 $checksumType = 'sha256'
-$checksum = '91430BA2CD4AE04260895B1080AE7965F64AEFA46C92CE64B26BDC59602B5F4F'
-$checksum64 = '33B136C49105CCBEA415038520EFBB2370F7B4A80B20BFF47F8AE5D155BB1C5D'
+$checksum = 'D7A45665A868172FB332522379165FBFAD04E43428FDA8ED57F77AB8790F66B7'
+$checksum64 = 'DB4A189DEA2734BB0DF3049AFA6A818FEDD6B862CBD2A54C955814411A31C88C'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType"
