fb7ff6996253616ad912d81100e4e229a5989771
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2017-02-22 15:09:41 +0100

    Updated checksum and description

--- sqlserver-odbcdriver/tools/chocolateyinstall.ps1
+++ sqlserver-odbcdriver/tools/chocolateyinstall.ps1
@@ -4,6 +4,6 @@ $silentArgs= '/Passive /NoRestart IACCEPTMSODBCSQLLICENSETERMS=YES'
 $url = 'https://download.microsoft.com/download/D/5/E/D5EEF288-A277-45C8-855B-8E2CB7E25B96/x86/msodbcsql.msi'
 $url64 = 'https://download.microsoft.com/download/D/5/E/D5EEF288-A277-45C8-855B-8E2CB7E25B96/x64/msodbcsql.msi'
 $checksumType = 'sha256'
-$checksum = 'B9B5E08D0E53D075FC24AD75FFD8EFC0FD0B36248991B2F2EC823E5A7165B1E3'
-$checksum64 = '43A0461DA90DE02B795BEDBF5E135857E0229B97F8C014847A1F404497428D94'
+$checksum = '3A3B3B83594A28CDA3ED9F19918AA53D6866073AEAC4E25D8069CBD058732C68'
+$checksum64 = '14C3C64468F75AF7EBEE2B9CF4F34A1E87365E973C379F33022E27683A837BCD'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType"
