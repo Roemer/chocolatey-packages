cde56787bd8d04ac7c0efeee22bdd71f913168b4
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2017-05-31 17:45:16 +0200

    Updated flyway.commandline

--- flyway.commandline/tools/chocolateyInstall.ps1
+++ flyway.commandline/tools/chocolateyInstall.ps1
@@ -1,6 +1,8 @@
-﻿$version = '4.0.3'
+﻿$version = '4.2.0'
 $packageName = 'flyway.commandline'
-$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage $packageName $url $toolsDir
+$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
+$checksumType = 'sha256'
+$checksum = 'B7F3AB0FFC7AB366F01AC42A518A3E56F96B3228B7B3C7ED31C9515DD5B7242F'
+Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
