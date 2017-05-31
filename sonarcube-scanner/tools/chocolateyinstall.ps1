29030ec61f91eaac9a1086c28241a535205a5b2f
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2017-05-31 17:11:00 +0200

    Updated sonarqube-scanner

--- sonarcube-scanner/tools/chocolateyinstall.ps1
+++ sonarcube-scanner/tools/chocolateyinstall.ps1
@@ -1,6 +1,8 @@
 ﻿$packageName = 'sonarcube-scanner'
-$url = 'https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.8.zip'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage $packageName $url $toolsDir
-Install-BinFile "sonar-runner" "$toolsDir\sonar-scanner-2.8\bin\sonar-runner.bat"
-Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-2.8\bin\sonar-scanner.bat"
+$url = 'https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.0.3.778.zip'
+$checksumType = 'sha256'
+$checksum = '4E3035F208548621433C8713DE5E536AA81AE1AB4DF2998E041B9236B3BA3170'
+Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
+Install-BinFile "sonar-runner" "$toolsDir\sonar-scanner-3.0.3.778\bin\sonar-runner.bat"
+Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-3.0.3.778\bin\sonar-scanner.bat"
