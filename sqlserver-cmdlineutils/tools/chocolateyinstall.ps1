2880422a84c3c12e974da752834ed7be5bea00da
Bäriswyl Roman <roman.baeriswyl@elca.ch>
2018-02-21 08:41:27 +0100

    Updated sqlserver-cmdlineutils to 14.0

--- sqlserver-cmdlineutils/tools/chocolateyinstall.ps1
+++ sqlserver-cmdlineutils/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'sqlserver-cmdlineutils'
 $installerType = 'msi'
 $silentArgs= '/Passive /NoRestart IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES'
-$url = 'https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/Command%20Line%20Utilities%20MSI%20files/x86/MsSqlCmdLnUtils.msi'
-$url64 = 'https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/Command%20Line%20Utilities%20MSI%20files/amd64/MsSqlCmdLnUtils.msi'
+$url = 'https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/x86/MsSqlCmdLnUtils.msi'
+$url64 = 'https://download.microsoft.com/download/C/8/8/C88C2E51-8D23-4301-9F4B-64C8E2F163C5/x64/MsSqlCmdLnUtils.msi'
 $checksumType = 'sha256'
-$checksum = 'D7A45665A868172FB332522379165FBFAD04E43428FDA8ED57F77AB8790F66B7'
-$checksum64 = 'DB4A189DEA2734BB0DF3049AFA6A818FEDD6B862CBD2A54C955814411A31C88C'
+$checksum = 'E871080C929036C528133C56BA4CEF7C35617F31812F877012BE9189B60D11BE'
+$checksum64 = '9479810A2EA8954E0CACD3D47FE0BCB81647B9CFB5EFD6AB79B76A6CCADCEFE7'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType"
