198fa0a8eacc9a1d2d87c0194fc687db4abf2607
Matt Hartfield <hartfiem@wwt.com>
2018-11-16 15:36:01 -0600

    flyway - updated to 5.2.1 - MH

--- flyway.commandline/tools/chocolateyInstall.ps1
+++ flyway.commandline/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$version = '4.2.0'
+﻿$version = '5.2.1'
 $packageName = 'flyway.commandline'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
 $checksumType = 'sha256'
-$checksum = 'B7F3AB0FFC7AB366F01AC42A518A3E56F96B3228B7B3C7ED31C9515DD5B7242F'
+$checksum = 'FA83263AFB1309477F5A6B064A29E039C19092CE713F9332E72C5017308E9CEA'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
