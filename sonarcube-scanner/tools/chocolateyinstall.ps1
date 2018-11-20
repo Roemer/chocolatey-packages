98499b945fa8cab0d7de106a58e471f50de7bf03
Roman Bäriswyl <roman.baeriswyl@elca.ch>
2018-11-20 07:44:44 +0100

    Made sonar-scanner a download package again due to size limits

--- sonarcube-scanner/tools/chocolateyinstall.ps1
+++ sonarcube-scanner/tools/chocolateyinstall.ps1
@@ -1,18 +1,12 @@
-﻿$ErrorActionPreference = 'Stop'
- 
-$packageName = 'sonarcube-scanner'
-$toolsPath  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installDir = "$toolsPath\sonar-scanner-3.2.0.1227-windows"
-
-# Unpack the zip file
-$packageArgs = @{
-    PackageName  = $packageName
-    File         = "$toolsPath\sonar-scanner-cli-3.2.0.1227-windows.zip"
-    Destination  = $toolsPath
-}
-Get-ChocolateyUnzip @packageArgs
+﻿$packageName = 'sonarcube-scanner'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-windows.zip'
+$checksumType = 'sha256'
+$checksum = '5CC23EE0CB8E8B09793EEF05CFB1B091EE05265E275A89846E476E630E087E05'
+Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 
 # Generate ignoring files to skip automatic generation of shims
+$installDir = "$toolsDir\sonar-scanner-3.2.0.1227-windows"
 $files = Get-ChildItem "$installDir\jre\bin" -include *.exe -recurse
 foreach ($file in $files) {
     #generate an ignore file
@@ -20,7 +14,4 @@ foreach ($file in $files) {
 }
 
 # Install the wanted shim
-Install-BinFile "sonar-scanner" "$installDir\bin\sonar-scanner.bat"
-
-# Remove the zip file
-Remove-Item -force "$toolsPath\*.zip" -ea 0
+Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-3.2.0.1227-windows\bin\sonar-scanner.bat"
