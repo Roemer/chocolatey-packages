d359803f6aeb17de6c848f6342c67738845cd02f
F.D.Castel <fdcastel@gmail.com>
2018-11-21 21:05:39 -0200

    Upgrade sqlserver-odbcdriver to version 17.2.0.1.

--- sqlserver-odbcdriver/tools/chocolateyinstall.ps1
+++ sqlserver-odbcdriver/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'sqlserver-odbcdriver'
 $installerType = 'msi'
 $silentArgs= '/Passive /NoRestart IACCEPTMSODBCSQLLICENSETERMS=YES'
-$url = 'https://download.microsoft.com/download/D/5/E/D5EEF288-A277-45C8-855B-8E2CB7E25B96/x86/msodbcsql.msi'
-$url64 = 'https://download.microsoft.com/download/D/5/E/D5EEF288-A277-45C8-855B-8E2CB7E25B96/x64/msodbcsql.msi'
+$url = 'https://download.microsoft.com/download/E/6/B/E6BFDC7A-5BCD-4C51-9912-635646DA801E/en-US/msodbcsql_17.2.0.1_x86.msi'
+$url64 = 'https://download.microsoft.com/download/E/6/B/E6BFDC7A-5BCD-4C51-9912-635646DA801E/en-US/msodbcsql_17.2.0.1_x64.msi'
 $checksumType = 'sha256'
-$checksum = '3A3B3B83594A28CDA3ED9F19918AA53D6866073AEAC4E25D8069CBD058732C68'
-$checksum64 = '14C3C64468F75AF7EBEE2B9CF4F34A1E87365E973C379F33022E27683A837BCD'
+$checksum = 'F52B68F2700E882D4BFFB47ACB440AF3AF0C82F5584A60242C5D5D69BF1170D1'
+$checksum64 = 'A5A315A7B47A17C56FE6E1185AC09C069594C13CF2343D453090D1C066AD2AEF'
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType"
