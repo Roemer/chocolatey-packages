65934ef6b12782f7f7028410a7da0922a9f08799
Balázs Keresztury <b@belidzs.hu>
2019-03-16 16:42:05 +0100

    Upgrade flyway.commandline to v5.2.4

--- flyway.commandline/tools/chocolateyInstall.ps1
+++ flyway.commandline/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$version = '5.2.1'
+﻿$version = '5.2.4'
 $packageName = 'flyway.commandline'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
+$url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version-windows-x64.zip"
 $checksumType = 'sha256'
-$checksum = 'FA83263AFB1309477F5A6B064A29E039C19092CE713F9332E72C5017308E9CEA'
+$checksum = '634a202ca73f43cf5f86741405631ada5739d3161ad6618d65973b1478d76e98'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
