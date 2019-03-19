1cb9bfa5ecb60e9896eb04b251206cfc99149392
Roman <r.baeriswyl@gmail.com>
2019-03-19 10:38:02 +0100

    Added basic cake to generate packages, cleanup of packages

--- opencover-portable/tools/chocolateyInstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$packageName = 'opencover.portable'
-$url = 'https://github.com/OpenCover/opencover/releases/download/4.6.519/opencover.4.6.519.zip'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage $packageName $url $toolsDir
--- sonarqube-scanner.portable/tools/chocolateyinstall.ps1
+++ sonarqube-scanner.portable/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
-﻿$packageName = 'sonarcube-scanner'
+﻿$packageName = 'sonarqube-scanner.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-windows.zip'
+$url = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-windows.zip'
 $checksumType = 'sha256'
-$checksum = '5CC23EE0CB8E8B09793EEF05CFB1B091EE05265E275A89846E476E630E087E05'
+$checksum = '9dddacd79ae5c6ac7805a309e1cfb753f59ed4bc2a09c761e61e472d2fce747d'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 
 # Generate ignoring files to skip automatic generation of shims
-$installDir = "$toolsDir\sonar-scanner-3.2.0.1227-windows"
+$installDir = "$toolsDir\sonar-scanner-3.3.0.1492-windows"
 $files = Get-ChildItem "$installDir\jre\bin" -include *.exe -recurse
 foreach ($file in $files) {
     #generate an ignore file
@@ -14,4 +14,4 @@ foreach ($file in $files) {
 }
 
 # Install the wanted shim
-Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-3.2.0.1227-windows\bin\sonar-scanner.bat"
+Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-3.3.0.1492-windows\bin\sonar-scanner.bat"
