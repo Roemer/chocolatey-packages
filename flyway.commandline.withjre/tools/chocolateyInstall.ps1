abe71556299fedc3cc6ed77e87ed6552736834c6
Balázs Keresztury <b@belidzs.hu>
2019-05-30 12:25:09 +0200

    Upgraded flyway to 6.0.0-beta2

--- flyway.commandline.withjre/tools/chocolateyInstall.ps1
+++ flyway.commandline.withjre/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$version = '6.0.0-beta'
+﻿$version = '6.0.0-beta2'
 $packageName = 'flyway.commandline.withjre'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version-windows-x64.zip"        
 $checksumType = 'sha256'
-$checksum = '2521588ef2af3b93394fef88c9c3a382d43c9a25a8db3f7a7ad935add6032bd9'
+$checksum = '19229da5ab1248b712c33757f56a3801b285826b3d5490da531ff648955cd574'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
--- flyway.commandline/tools/chocolateyInstall.ps1
+++ flyway.commandline/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-﻿$version = '6.0.0-beta'
+﻿$version = '6.0.0-beta2'
 $packageName = 'flyway.commandline'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$version/flyway-commandline-$version.zip"
 $checksumType = 'sha256'
-$checksum = '9a222b634da2636dfe1f9b4e561b56713c24ce3ed4da246a2907144c1d2a7740'
+$checksum = '73c38a8534dc035e7703c49c5bc234deaf09110b2151f3b4923619400273581f'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 Install-BinFile "flyway" "$toolsDir\flyway-$version\flyway.cmd"
