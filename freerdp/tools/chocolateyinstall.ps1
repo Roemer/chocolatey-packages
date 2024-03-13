4c35365c3a5e464efab15f21b78277ae795c21be
Roman Bäriswyl <r.baeriswyl@gmail.com>
2024-03-13 10:12:51 +0100

    Updated Flyway, deprecated FreeRDP

--- freerdp/tools/chocolateyinstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$packageName = 'freerdp'
-$url = 'https://www.cloudbase.it/downloads/wfreerdp-1.1.zip'
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Install-ChocolateyZipPackage $packageName $url $toolsDir
