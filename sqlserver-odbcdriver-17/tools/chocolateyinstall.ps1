e3fc897d662e1a706df8602a8ac94f6e0ce0d3fe
Roman Bäriswyl <r.baeriswyl@gmail.com>
2024-03-13 13:18:53 +0100

    Fix package

--- sqlserver-odbcdriver-17/tools/chocolateyinstall.ps1
+++ sqlserver-odbcdriver-17/tools/chocolateyinstall.ps1
@@ -12,8 +12,17 @@ $64DllPath = Join-Path -Path $Env:SystemRoot -ChildPath (Join-Path -Path 'syswow
 $32BitNeeded = ([Version]$(Get-ItemProperty -Path $32DllPath -ErrorAction:Ignore).VersionInfo.ProductVersion) -lt [Version]$Env:ChocolateyPackageVersion  
 $64BitNeeded = ([Version]$(Get-ItemProperty -Path $64DllPath -ErrorAction:Ignore).VersionInfo.ProductVersion) -lt [Version]$Env:ChocolateyPackageVersion
 
-$UpdateNeeded = ($32BitNeeded -or $64BitNeeded -or $Env:ChocolateyForce)
-if ($UpdateNeeded)
+$updateNeeded = ($32BitNeeded -or $64BitNeeded -or $Env:ChocolateyForce)
+if ($updateNeeded)
 {
-    Install-ChocolateyPackageCmdlet "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType" -UseOriginalLocation
+    Install-ChocolateyPackage -PackageName "$packageName" `
+        -FileType "$installerType" `
+        -SilentArgs "$silentArgs" `
+        -Url "$url" `
+        -Url64bit "$url64" `
+        -ValidExitCodes @(0) `
+        -Checksum "$checksum" `
+        -ChecksumType "$checksumType" `
+        -Checksum64 "$checksum64" `
+        -ChecksumType64 "$checksumType";
 }
--- sqlserver-odbcdriver-18/tools/chocolateyinstall.ps1
+++ sqlserver-odbcdriver-18/tools/chocolateyinstall.ps1
@@ -5,15 +5,24 @@ $url = '{link32}'
 $url64 = '{link64}'
 $checksumType = 'sha256'
 $checksum = '{checksum32}'
-$checksum64 = '{checksum64}';
+$checksum64 = '{checksum64}'
 
 $32DllPath = Join-Path -Path $Env:SystemRoot -ChildPath (Join-Path -Path 'system32' -ChildPath 'msodbcsql18.dll')
 $64DllPath = Join-Path -Path $Env:SystemRoot -ChildPath (Join-Path -Path 'syswow64' -ChildPath 'msodbcsql18.dll')
 $32BitNeeded = ([Version]$(Get-ItemProperty -Path $32DllPath -ErrorAction:Ignore).VersionInfo.ProductVersion) -lt [Version]$Env:ChocolateyPackageVersion  
 $64BitNeeded = ([Version]$(Get-ItemProperty -Path $64DllPath -ErrorAction:Ignore).VersionInfo.ProductVersion) -lt [Version]$Env:ChocolateyPackageVersion
 
-$UpdateNeeded = ($32BitNeeded -or $64BitNeeded -or $Env:ChocolateyForce)
-if ($UpdateNeeded)
+$updateNeeded = ($32BitNeeded -or $64BitNeeded -or $Env:ChocolateyForce)
+if ($updateNeeded)
 {
-    Install-ChocolateyPackageCmdlet "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes @(0) "$checksum" "$checksumType" "$checksum64" "$checksumType" -UseOriginalLocation
+    Install-ChocolateyPackage -PackageName "$packageName" `
+        -FileType "$installerType" `
+        -SilentArgs "$silentArgs" `
+        -Url "$url" `
+        -Url64bit "$url64" `
+        -ValidExitCodes @(0) `
+        -Checksum "$checksum" `
+        -ChecksumType "$checksumType" `
+        -Checksum64 "$checksum64" `
+        -ChecksumType64 "$checksumType";
 }
