feb1041920e5920db25cae4b6d343a9249c2d527
Roman Bäriswyl <r.baeriswyl@gmail.com>
2024-11-13 10:46:52 +0100

    Fixed sonar scanner download

--- sonarqube-scanner.portable/tools/chocolateyinstall.ps1
+++ sonarqube-scanner.portable/tools/chocolateyinstall.ps1
@@ -1,17 +1,17 @@
 ﻿$packageName = 'sonarqube-scanner.portable'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-{version}-windows.zip'
+$url = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-{version}-windows-x64.zip'
 $checksumType = 'sha256'
 $checksum = '{checksum}'
 Install-ChocolateyZipPackage $packageName $url $toolsDir -Checksum $checksum -ChecksumType $checksumType
 
 # Generate ignoring files to skip automatic generation of shims
-$installDir = "$toolsDir\sonar-scanner-{version}-windows"
+$installDir = "$toolsDir\sonar-scanner-{version}-windows-x64"
 $files = Get-ChildItem "$installDir\jre\bin" -include *.exe -recurse
 foreach ($file in $files) {
-    #generate an ignore file
+    # Generate an ignore file
     New-Item "$file.ignore" -type file -force | Out-Null
 }
 
 # Install the wanted shim
-Install-BinFile "sonar-scanner" "$toolsDir\sonar-scanner-{version}-windows\bin\sonar-scanner.bat"
+Install-BinFile "sonar-scanner" "$installDir\bin\sonar-scanner.bat"
